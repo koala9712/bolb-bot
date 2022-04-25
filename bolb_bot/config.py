4e9896bc6fd98904f831d302ec813416c87f08a9
ooliver1 <oliverwilkes2006@icloud.com>
2022-04-25 19:44:45 +0100

    pls

--- bolb_bot/config.py
+++ bolb_bot/config.py
@@ -2,7 +2,7 @@ from itertools import product
 
 
 # get all combos of cases
-low = "bolb"
+low = "bolb "
 up = low.upper()
 m = zip(low, up)
 
@@ -11,3 +11,4 @@ db_name = "bolb"
 colors = [0x00FFEA]
 prefix = set("".join(m) for m in product(*m))
 helpindex = "bolb."
+helpprefix = True
