6bbe229fa3805e9c9e02f82fed0db0c1fe070307
ooliver1 <oliverwilkes2006@icloud.com>
2022-04-25 18:56:41 +0100

    chore: foroor changes

--- bolb_bot/__main__.py
+++ bolb_bot/__main__.py
@@ -18,13 +18,13 @@ class MyBot(BotBase):
     ...
 
 
-bot = MyBot(intents=intents, config_module="bolb-bot.config")
+bot = MyBot(intents=intents, config_module="bolb_bot.config")
 
 
 if __name__ == "__main__":
-    for filename in listdir("bolb-bot/cogs"):
+    for filename in listdir("bolb_bot/cogs"):
         if filename.endswith(".py"):
-            bot.load_extension(f"bolb-bot.cogs.{filename[:-3]}")
+            bot.load_extension(f"bolb_bot.cogs.{filename[:-3]}")
         else:
             if isfile(filename):
                 print(f"Unable to load {filename[:-3]}")
